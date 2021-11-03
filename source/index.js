29c871935253ff27387c5ad02fb3a55b7f97393f
Kaique Pereira Leite <54877141+kaiquepl@users.noreply.github.com>
2021-11-03 05:54:40 -0300

    Atualização da Tabela
    Atualizei a tabela para a que eu estou usando no site AWS, para Product3!

--- source/index.js
+++ source/index.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
         requestJSON = JSON.parse(event.body);
         await dynamo
           .put({
-            TableName: "Products",
+            TableName: "Product3",
             Item: {
               id: requestJSON.id,
               price: requestJSON.price,
@@ -32,7 +32,7 @@ exports.handler = async (event, context) => {
         console.log(event.pathParameters.id)
         await dynamo
           .delete({
-            TableName: "Products",
+            TableName: "Product3",
             Key: {
               id: event.pathParameters.id
             }
@@ -43,7 +43,7 @@ exports.handler = async (event, context) => {
       case "GET /items/{id}":
         body = await dynamo
           .get({
-            TableName: "Products",
+            TableName: "Product3",
             Key: {
               id: event.pathParameters.id
             }
@@ -51,13 +51,13 @@ exports.handler = async (event, context) => {
           .promise();
         break;
       case "GET /items":
-        body = await dynamo.scan({ TableName: "Products" }).promise();
+        body = await dynamo.scan({ TableName: "Product3" }).promise();
         break;
       case "PUT /items/{id}":
          requestJSON = JSON.parse(event.body);
         await dynamo
           .update({
-            TableName: "Products",
+            TableName: "Product3",
             Key: {
               id: event.pathParameters.id
             },
